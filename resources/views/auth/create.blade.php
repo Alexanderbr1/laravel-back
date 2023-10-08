4cb38490a606432cb83c171d2e6b7eaa2f78b38e
Alexanderbr1 <bryantzev2004@mail.ru>
2023-10-08 21:53:31 +0300

    auth without btn)

--- resources/views/auth/create.blade.php
+++ resources/views/auth/create.blade.php
@@ -2,29 +2,29 @@
 @section('content')
 <form action="/registr" method="post">
     @csrf
-    @if ($errors -> any())
-    <div class="alert-danger">
-    @foreach($errors -> all() as $error)
-    <ul>
-        <li>{{$error}}</li>
-    </ul>
-    @endforeach
-    </div>
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        @foreach($errors->all() as $error)
+        <ul>
+            <li>{{$error}}</li>
+        </ul>
+        @endforeach
+    </div>    
     @endif
-<div class="mb-3">
+<div class="form-group">
     <label for="name">Name</label>
     <input type="text" class="form-control" name="name" id="name">
   </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
     <input type="email" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1"  class="form-label">Password</label>
-    <input type="password" name="password" class="form-control" id="exampleInputPassword1">
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" name="password" id="exampleInputPassword1">
   </div>
-  <div class="mb-3 form-check">
+  <div class="form-group form-check">
     <input type="checkbox" class="form-check-input" id="exampleCheck1">
     <label class="form-check-label" for="exampleCheck1">Check me out</label>
   </div>
