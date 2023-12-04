c9bdd606340d4a6ce39acecc0b46bcf2143c87bd
Alexanderbr1 <bryantzev2004@mail.ru>
2023-12-05 00:00:37 +0300

    cache part 1

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Comment;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Notification;
 use App\Mail\AdminComment;
 use App\Models\Article;
@@ -26,6 +28,12 @@ class CommentController extends Controller
         $comment->accept = true;
         $res = $comment->save();
         $article = Article::where('id', $comment->article_id)->first();
+
+        $caches = DB::table('cache')->whereRaw('`key` GLOB :key',  [':key'=> 'article/*[0-9]:[0-9]'])->get();
+        foreach($caches as $cache){
+            Cache::forget($cache->key);
+        }
+
         if($res) {
             $users = User::where('id', '!=', auth()->id())->get();
             Notification::send($users, new CommentNotifi($article));
@@ -34,6 +42,12 @@ class CommentController extends Controller
     }
 
     public function reject(int $id){
+
+        $caches = DB::table('cache')->whereRaw('`key` GLOB :key',  [':key'=> 'article/*[0-9]:[0-9]'])->get();
+        foreach($caches as $cache){
+            Cache::forget($cache->key);
+        }
+
         $comment = Comment::findOrFail($id);
         $comment->accept = false;
         $comment->save();
@@ -77,6 +91,12 @@ class CommentController extends Controller
     }
 
     public function delete($id){
+
+        $caches = DB::table('cache')->whereRaw('`key` GLOB :key',  [':key'=> 'article/*[0-9]:[0-9]'])->get();
+        foreach($caches as $cache){
+            Cache::forget($cache->key);
+        }
+
         $comment = Comment::findOrFail($id);
         Gate::authorize('comment', $comment);
         $comment->delete();
